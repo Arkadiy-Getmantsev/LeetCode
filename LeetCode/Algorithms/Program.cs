b34bf8e53c70e26d581bfc22120d7db15098d9f4
Arkadiy Getmantsev <arkadiy.getmantsev@evolvice.de>
2018-05-22 14:48:00 +0300

    1. TwoSum

---
+++ LeetCode/Algorithms/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result1 = TwoSum1.TwoSum(new int[] { 2, 7, 11, 15 }, 9);
+        }
+    }
+}
