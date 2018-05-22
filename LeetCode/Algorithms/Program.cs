c3434a1a071cbf7e3d43bbea6935d9275b7df129
Arkadiy Getmantsev <arkadiy.getmantsev@evolvice.de>
2018-05-22 16:33:30 +0300

    2. Add Two Numbers

--- LeetCode/Algorithms/Program.cs
+++ LeetCode/Algorithms/Program.cs
@@ -11,6 +11,21 @@ namespace Algorithms
         static void Main(string[] args)
         {
             var result1 = TwoSum1.TwoSum(new int[] { 2, 7, 11, 15 }, 9);
+            var result2 = AddTwoNumbers2.AddTwoNumbers(
+                new ListNode(2)
+                {
+                    next = new ListNode(4)
+                    {
+                        next = new ListNode(3)
+                    }
+                },
+                new ListNode(5)
+                {
+                    next = new ListNode(6)
+                    {
+                        next = new ListNode(4)
+                    }
+                });
         }
     }
 }
